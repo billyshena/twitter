cddd1751a8e3df889f5201f4dcdc98a76c3b7fdc
Billy <bshen@ibsciss.com>
2014-12-09 16:47:55 +0100

    [FIX] Profile

--- frontend/src/js/controllers/followerCtrl.js
+++ frontend/src/js/controllers/followerCtrl.js
@@ -1,13 +1,14 @@
 /**
  * Created by bshen on 09/12/14.
  */
-angular.module('app.controllers.following', []).controller('followingCtrl', [
+angular.module('app.controllers.follower', []).controller('followerCtrl', [
     '$scope', '$http', 'Logger', 'Storage',
     function ($scope, $http, Logger, Storage) {
 
         /* Initialize scope values */
         $scope.current_user = JSON.parse(Storage.get('token')).id;
         $scope.users = [];
+
 
         /* Get current user */
         $http
@@ -17,15 +18,14 @@ angular.module('app.controllers.following', []).controller('followingCtrl', [
             }, function(err){
                 console.log(err);
             });
-
+        
         /* Get the following users */
         $http
-            .get(appConfig.appUrl + '/user/' + $scope.current_user + '/following')
+            .get(appConfig.appUrl + '/user/' + $scope.current_user + '/followers')
             .then(function(response){
                 $scope.users = response.data;
             }, function(err){
                 console.log(err);
             });
-
     }
 ]);
