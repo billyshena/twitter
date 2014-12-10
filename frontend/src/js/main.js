9b9180fac6e73ef21b25355857a38532fc2a7016
Billy <bshen@ibsciss.com>
2014-12-10 17:13:21 +0100

    u

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -12,7 +12,7 @@ angular.module('app.controllers.main',[]).controller('AppCtrl', [
         $scope.selected = undefined;
         $scope.avatarUrl = appConfig.appUrl + '/avatars';
         $scope.isOpen = false;
-        $scope.current_user = JSON.parse(Storage.get('token')).id;
+
 
 
 
@@ -31,6 +31,8 @@ angular.module('app.controllers.main',[]).controller('AppCtrl', [
 
 
         if(Auth.isAuthenticated()){
+            $scope.current_user = JSON.parse(Storage.get('token')).id;
+            
             $http.get(appConfig.appUrl + '/user/' + JSON.parse(Storage.get('token')).id).success(function(data){
                 $scope.user = data;
             }).error(function(err){
